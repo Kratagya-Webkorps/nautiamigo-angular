0d344b5fd1e3a0fa0ea7440c6145ec5630144b12
Suvadeep <suvadeep@webkorps.com>
2025-08-14 16:22:57 +0530

    ng ERror fixed

--- src/app/testimonials/testimonials.component.ts
+++ src/app/testimonials/testimonials.component.ts
@@ -30,31 +30,31 @@ export class TestimonialsComponent implements OnInit {
     {
       id: 1,
       quote: "Had a great experience....went with my family....better than other cruise as I have tried other one as well last year....they have Air conditioner Vip lounge as well, fun activities for kids, couples, dance, good food.....can surely try once if you want to have dinner cruise experience....crew is also very nice and helpful.",
-      name: "Charul Arora",
+      name: "CHARUL ARORA",
       title: "Guest review",
-      image: "../../assets/img/team/4.jpg",
+      image: "assets/img/team/4.jpg",
       rating: 5
     },
     {
       id: 2,
-      quote: "Great experience. Was there on the occasion of my wife's bday and asked the hosts for a cake 10 mins before departure. Still the hosts especially Shelton was too helpful and got the same arranged. Thank you so much guys for making this happen. Wonderful experience and had lots of fun. Definitely recommended.",
-      name: "Sarvesh Kumar Rai",
-      title: "Guest review",
-      image: "../../assets/img/team/1.jpg",
+      quote: "Had an amazing time at nauti Amigo cruise, The vip lounge was fantastic, and the food was spot on. Big thanks to the team for their support. Truly a memorable experience Highly recommend.",
+      name: "DOLLY BAJAJ",
+      title: "Guest review", 
+      image: "assets/img/team/5.jpg",
       rating: 5
     },
     {
       id: 3,
-      quote: "Had an amazing time at nauti Amigo cruise, The vip lounge was fantastic, and the food was spot on. Big thanks to the team for their support. Truly a memorable experience Highly recommend.",
-      name: "Dolly Bajaj",
+      quote: "Great experience. Was there on the occasion of my wife's bday and asked the hosts for a cake 10 mins before departure. Still the hosts especially Shelton was too helpful and got the same arranged. Thank you so much guys for making this happen. Wonderful experience and had lots of fun. Definitely recommended.",
+      name: "SARVESH KUMAR RAI",
       title: "Guest review",
-      image: "../../assets/img/team/5.jpg",
+      image: "assets/img/team/1.jpg", 
       rating: 5
     }
   ];
 
   ngOnInit(): void {
-    // Swiper configuration can be set here if needed
+    // Component initialization
   }
 
   generateStars(rating: number): number[] {
