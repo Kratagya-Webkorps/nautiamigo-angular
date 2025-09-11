ef9b935fc6f80032ba1f2a04b6f05d5bd5b4aee9
Suvadeep <suvadeep@webkorps.com>
2025-09-11 16:13:31 +0530

    made some changes to make the navbar transparent in the hero section

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
+  standalone: true,
   imports: [],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  
+  @Input() transparent = false;
 
+  
+  isHero = false;
+
+  constructor(private router: Router) {
+    
+    this.isHero = this.isHeroUrl(this.router.url);
+    
+    this.router.events.subscribe((evt) => {
+      if (evt instanceof NavigationEnd) {
+        this.isHero = this.isHeroUrl(evt.urlAfterRedirects || evt.url);
+      }
+    });
+  }
+
+  private isHeroUrl(url: string): boolean {
+  
+    return url === '/' || url === '';
+  }
 }
 
