ec699254a281684898e9067ab48512473045eb58
Suvadeep <suvadeep@webkorps.com>
2025-09-15 14:16:51 +0530

    make navbar transparent at top and visible for rest of pages

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,24 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { Component, HostListener, Input } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-  
   @Input() transparent = false;
-
-  
   isHero = false;
+  atTop = true; 
 
   constructor(private router: Router) {
-    
     this.isHero = this.isHeroUrl(this.router.url);
-    
+
     this.router.events.subscribe((evt) => {
       if (evt instanceof NavigationEnd) {
         this.isHero = this.isHeroUrl(evt.urlAfterRedirects || evt.url);
@@ -27,8 +23,12 @@ export class NavbarComponent {
   }
 
   private isHeroUrl(url: string): boolean {
-  
     return url === '/' || url === '';
   }
-}
 
+  // detect scroll
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.atTop = window.scrollY < 50; 
+  }
+}
